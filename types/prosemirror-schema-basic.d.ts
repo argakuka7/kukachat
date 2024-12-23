d5862f3a01e12a25d87802c790d1d344fd0765c0
argakuka <kukaarga@gmail.com>
2024-12-23 11:03:23 +0700

    lagi5

--- types/prosemirror-schema-basic.d.ts
+++ types/prosemirror-schema-basic.d.ts
@@ -1,4 +1,24 @@
 declare module 'prosemirror-schema-basic' {
-  import { Schema } from 'prosemirror-model';
-  export const schema: Schema;
+  import { NodeSpec, MarkSpec, Schema } from 'prosemirror-model';
+  
+  interface BasicSchema extends Schema {
+    nodes: {
+      doc: NodeSpec;
+      paragraph: NodeSpec;
+      text: NodeSpec;
+      heading: NodeSpec;
+      blockquote: NodeSpec;
+      horizontal_rule: NodeSpec;
+      code_block: NodeSpec;
+      hard_break: NodeSpec;
+    };
+    marks: {
+      link: MarkSpec;
+      em: MarkSpec;
+      strong: MarkSpec;
+      code: MarkSpec;
+    };
+  }
+
+  export const schema: BasicSchema;
 } 
